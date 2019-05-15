81925c1a204d4d9a968829e45d36984e745c47b6
kilpatty <seanpkilgarriff@gmail.com>
2019-05-15 17:59:20 -0500

    remove unused code

--- src/primitives/address.rs
+++ src/primitives/address.rs
@@ -2,13 +2,11 @@ use blake2::Blake2b;
 
 pub struct Address {
     pub version: u32,
-    pub hash: Blake2b
+    pub hash: Blake2b,
 }
 
-impl From<String> for Address {
-    fn from(item: i32) -> Self {
-        Number { value: item }
-    }
-}
-
-
+// impl From<String> for Address {
+//     fn from(item: i32) -> Self {
+//         Address { value: item }
+//     }
+// }
