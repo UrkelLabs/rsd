f323fa32f9a2fc3b880bc87a12000de30d02988a
kilpatty <seanpkilgarriff@gmail.com>
2019-05-21 15:15:24 -0500

    initial of output for transactions

---
+++ src/primitives/output.rs
@@ -0,0 +1,14 @@
+use crate::{Address, Amount, Covenant};
+
+pub struct Output {
+    value: Amount,
+    address: Address,
+    covenant: Covenant,
+}
+
+//TODO get size, is_dust, format, equal + peq, to hex from hex, to buffer, from buffer.
+impl Output {
+    pub fn is_unspendable(&self) -> bool {
+        self.address.is_unspendable() | self.covenant.is_unspendable()
+    }
+}
