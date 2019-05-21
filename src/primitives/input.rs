ab34120556a15dc8e9122b00edd97e9d8d1e4248
kilpatty <seanpkilgarriff@gmail.com>
2019-05-21 15:43:30 -0500

    initial for input

---
+++ src/primitives/input.rs
@@ -0,0 +1,8 @@
+use crate::Outpoint;
+
+pub struct Input {
+    pub prevout: Outpoint,
+    pub sequence: u32,
+    //TODO this should probably be a custom type, but can be implemented later.
+    pub witness: Vec<Vec<u8>>,
+}
--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -2,6 +2,7 @@ pub mod address;
 // pub mod block;
 pub mod covenant;
 pub mod headers;
+pub mod input;
 pub mod outpoint;
 pub mod output;
 pub mod transaction;
