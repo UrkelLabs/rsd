cd462a310b4c9bab72cc128950acaa1adfea9f06
kilpatty <seanpkilgarriff@gmail.com>
2019-05-21 13:01:43 -0500

    lots of covenant work -> convert them to new types

--- src/primitives/covenant.rs
+++ src/primitives/covenant.rs
@@ -1,13 +1,15 @@
 use crate::Address;
-use blake2::Blake2b;
+use crate::{Hash, Name, NameHash, Uint256};
 
 /// A Handshake covenant, which is a method of changing name state on the chain.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub enum Covenant {
-    Empty,
-    Claim,
-    Bid,
-    Open,
+    Empty(EmptyCovenant),
+    Claim(ClaimCovenant),
+    Bid(BidCovenant),
+    Open(OpenCovenant),
+    Reveal(RevealCovenant),
+    Register(RegisterCovenant),
 }
 
 //TODO the methods that span all Covenants -> Like from, and various helpers
@@ -18,58 +20,62 @@ pub enum Covenant {
 /// A Empty Covenant is included in the case of a payment.
 /// It effects no change on the urkel tree.
 /// In the original codebase the type of this covenant would be "none"
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct EmptyCovenant {}
 
 //TODO thought -> We could also make this a type of "NoneCovenant"
 
-//TODO
-pub struct Claim {
-    pub name_hash: Blake2b,
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct ClaimCovenant {
+    pub name_hash: NameHash,
     pub height: u32,
-    pub name: String,
-    //TODO verify type on this.
+    pub name: Name,
     pub flags: String,
 }
 
-pub struct Open {
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct OpenCovenant {
     ///The hash of the name for the Open.
-    //check if this is the correct type for namehash.
-    pub name_hash: Blake2b,
+    pub name_hash: NameHash,
     ///The height at which the bid occured
     ///The height should always be 0 for an Open.
     pub height: u32,
     ///The raw name that the open is for.
-    pub name: String,
+    pub name: Name,
 }
 
-pub struct Bid {
-    pub name_hash: Blake2b,
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct BidCovenant {
+    pub name_hash: NameHash,
     pub height: u32,
-    pub name: String,
-    pub hash: Blake2b,
+    pub name: Name,
+    //TODO *might* want to make this a BidHash, but that'll be a later impl
+    pub hash: Hash,
 }
 
-pub struct Reveal {
-    pub name_hash: Blake2b,
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct RevealCovenant {
+    pub name_hash: NameHash,
     pub height: u32,
-    //TODO verify output type. This might also be better served as a Blake2b
-    pub nonce: String,
+    pub nonce: Uint256,
 }
 
-pub struct Redeem {
-    pub name_hash: Blake2b,
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct RedeemCovenant {
+    pub name_hash: NameHash,
     pub height: u32,
 }
 
-pub struct Register {
-    pub name_hash: Blake2b,
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct RegisterCovenant {
+    pub name_hash: NameHash,
     pub height: u32,
     //TODO verify type. I believe this is a serialized encoding of the data insert. We should make
     //this a cutom type.
     pub record_data: String,
-    //TODO Verify type. Not going to be a Blake2b (I think) Since we are using a different hashing
-    //method. Will have to grab a type from the crypto library when we finish it.
-    pub block_hash: String,
+    //TODO not sure if we want to have BlockHash custom type, but it might make sense.
+    //Otherwise Hash here is just fine
+    pub block_hash: Hash,
 }
 
 pub struct Update {
