f3b4e5c9095f166bcf6325cf5b0e507f45f8876a
kilpatty <seanpkilgarriff@gmail.com>
2019-05-21 13:00:57 -0500

    expose new types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,9 +1,11 @@
 pub mod amount;
 pub mod buffer;
 pub mod hash;
+pub mod name;
 pub mod uint;
 
 pub use amount::Amount;
 pub use buffer::Buffer;
-pub use hash::Hash;
+pub use hash::{Hash, NameHash};
+pub use name::Name;
 pub use uint::Uint256;
