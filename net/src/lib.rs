b2f48f71dea44ade6e06ee5b2ff42393945c9275
kilpatty <seanpkilgarriff@gmail.com>
2019-06-25 14:33:52 -0500

    net-lib: add result type that wraps error

--- net/src/lib.rs
+++ net/src/lib.rs
@@ -1,8 +1,11 @@
 #![feature(async_await)]
 
 pub mod common;
+pub mod error;
 pub mod net_address;
 pub mod packets;
 pub mod peer;
 pub mod pool;
 pub mod types;
+
+pub type Result<T> = std::result::Result<T, error::Error>;
