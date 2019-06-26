c6154a9001067375b68735d6aaca54dff4eaae86
kilpatty <seanpkilgarriff@gmail.com>
2019-06-26 12:48:46 -0500

    net-types-services: Revamp services to use bitflags

--- net/src/types/services.rs
+++ net/src/types/services.rs
@@ -2,46 +2,29 @@ use crate::common::{BLOOM, FULL_NODE, NETWORK};
 use crate::error;
 use std::convert::TryFrom;
 
-//Service Enum
-#[derive(Debug, Clone, Copy)]
-pub enum Services {
-    Unknown,
-    Network,
-    Bloom,
-    FullNode,
-    // RequiredServices,
-    // LocalServices,
-}
+use bitflags::bitflags;
 
-impl TryFrom<u32> for Services {
-    type Error = error::Error;
+bitflags! {
+    /// Options for what type of interaction a peer supports
+    pub struct Services: u32 {
+        /// We don't know (yet) what the peer can do.
+        const UNKNOWN = 0b00000000;
+        /// Service constant for Network capabilities (1 << 0)
+        const NETWORK = 0b00000001;
+        ///Service constant for Bloom Filter capabilities
+        const BLOOM = 0b00000010;
 
-    fn try_from(value: u32) -> Result<Self, Self::Error> {
-        let service = match value {
-            0 => Services::Unknown,
-            NETWORK => Services::Network,
-            BLOOM => Services::Bloom,
-            // FULL_NODE => Services::FullNode,
-            // REQUIRED_SERVICES =>, Services::RequiredServices,
-            // LOCAL_SERVICES =>
-            _ => return Err(error::Error::UnknownService),
-        };
+        pub const FULL_NODE = 0 | Services::NETWORK.bits;
+        pub const REQUIRED_SERVICES = 0 | Services::NETWORK.bits;
+        pub const LOCAL_SERVICES = 0 | Services::NETWORK.bits;
 
-        Ok(service)
-    }
-}
-
-impl Services {
-    pub fn value(&self) -> u32 {
-        match *self {
-            Services::Unknown => 0,
-            //1
-            Services::Network => NETWORK,
-            //2
-            Services::Bloom => BLOOM,
-            Services::FullNode => FULL_NODE,
-            // Services::RequiredServices => REQUIRED_SERVICES,
-            // Services::LocalServices => LOCAL_SERVICES,
-        }
+        /// All nodes right now are "full nodes".
+        /// Some nodes internally may maintain longer block histories (archival_mode)
+        /// but we do not advertise this to other nodes.
+        /// All nodes by default will accept lightweight "kernel first" tx broadcast.
+        const FULL_NODE = Capabilities::HEADER_HIST.bits
+            | Capabilities::TXHASHSET_HIST.bits
+            | Capabilities::PEER_LIST.bits
+            | Capabilities::TX_KERNEL_HASH.bits;
     }
 }
