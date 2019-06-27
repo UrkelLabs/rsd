cd08934d19fad6f32c4684b5f7bc40ef92a1fed0
kilpatty <seanpkilgarriff@gmail.com>
2019-06-27 14:10:22 -0500

    net-packets: implement remaining decoding for version packet

--- net/src/packets.rs
+++ net/src/packets.rs
@@ -1,11 +1,44 @@
 use crate::common::USER_AGENT;
 use crate::net_address::NetAddress;
 use crate::types::{ProtocolVersion, Services};
-use chrono::{DateTime, Utc};
-use handshake_protocol::encoding::Encodable;
-use handshake_types::Buffer;
+use crate::Result;
+use chrono::{DateTime, TimeZone, Utc};
+use extended_primitives::Buffer;
+use handshake_protocol::encoding::{Decodable, Encodable};
+use handshake_protocol::network::Network;
+use rand::Rng;
 
-#[derive(Copy, Clone)]
+pub enum Packet {
+    Version(VersionPacket),
+    Verack,
+}
+
+impl Packet {
+    pub fn parse(mut packet: Buffer) -> Result<(Buffer, u8)> {
+        let magic = packet.read_u32()?;
+        let _type = packet.read_u8()?;
+        let size = packet.read_u32()?;
+
+        //Check magic number, throw packet invalid magic number
+        //Check size, and ensure it's below constant max message size. -> We already have
+        //This checked in Brontide, but I think let's check it again here.
+
+        Ok((packet, _type))
+    }
+
+    pub fn decode(packet: Buffer) -> Result<Self> {
+        let (raw_packet, packet_type) = Packet::parse(packet)?;
+        match packet_type {
+            1 => {
+                let packet = VersionPacket::decode(raw_packet)?;
+                Ok(Packet::Version(packet))
+            }
+            _ => Ok(Packet::Verack),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 pub enum PacketType {
     Version = 0,
     Verack = 1,
@@ -42,6 +75,10 @@ pub enum PacketType {
     Data = 32,
 }
 
+//Optionally all packets go inside of a Packet enum and we use that to implement frame, which we
+//can remove from peer. Not sure if that's the way we want to go, but this should work for now.
+
+#[derive(Clone, Debug)]
 pub struct VersionPacket {
     _type: PacketType,
     version: ProtocolVersion,
@@ -49,7 +86,8 @@ pub struct VersionPacket {
     //Check on this.
     time: DateTime<Utc>,
     remote: NetAddress,
-    // nonce:
+    //This doesn't feel correct, probably should be a setBuffer TODO
+    nonce: Buffer,
     agent: String,
     height: u32,
     no_relay: bool,
@@ -58,128 +96,90 @@ pub struct VersionPacket {
 //Make Packet a trait, and have it include functions like size and encode.
 impl VersionPacket {
     pub fn new(addr: NetAddress, height: u32, no_relay: bool) -> Self {
+        //TODO we probably want to implement noncelist here.
+        let nonce = rand::thread_rng().gen::<[u8; 8]>();
         VersionPacket {
             _type: PacketType::Version,
             version: ProtocolVersion::default(),
-            services: Services::LocalServices,
+            services: Services::LOCAL_SERVICES,
             time: Utc::now(),
             remote: addr,
             agent: USER_AGENT.to_owned(),
+            nonce: Buffer::from(nonce.to_vec()),
             height,
             no_relay,
         }
     }
 
-    //TODO implement
-    //TODO maybe make this a fn rather than a method
-    pub fn size(&self) -> u32 {
-        0
-    }
-
-    pub fn encode(&self) {
+    //Function to frame the version packet.
+    //we should maybe make this a trait.
+    //Trait Packet
+    //should impl frame, and parse
+    pub fn frame(&self, network: Network) -> Buffer {
         let mut buffer = Buffer::new();
 
-        //Framing
-        //Write magic number here TODO
-        buffer.write_u32(0);
+        buffer.write_u32(network.magic());
         buffer.write_u8(self._type as u8);
         buffer.write_u32(self.size());
 
-        //Write remaning packet
+        //Write encoded packet
+        buffer.extend(self.encode());
+
+        buffer
+    }
+
+    fn decode(mut packet: Buffer) -> Result<Self> {
+        let version = packet.read_u32()?;
+        let services = packet.read_u32()?;
+        packet.read_u32()?;
+        let timestamp = packet.read_u64()?;
+        let remote = NetAddress::decode(&mut packet)?;
+        let nonce = packet.read_bytes(8)?;
+        let agent_length = packet.read_u8()?;
+        let agent = packet.read_string(agent_length as usize)?;
+        let height = packet.read_u32()?;
+        let no_relay = packet.read_u8()?;
+
+        Ok(VersionPacket {
+            _type: PacketType::Version,
+            version: ProtocolVersion::from(version),
+            services: Services::from_bits_truncate(services),
+            time: Utc.timestamp(timestamp as i64, 0),
+            remote,
+            agent,
+            nonce: Buffer::from(nonce),
+            height,
+            no_relay: no_relay == 1,
+        })
+    }
+}
+
+impl Encodable for VersionPacket {
+    fn size(&self) -> u32 {
+        let mut size = 0;
+        size += 20;
+        size += self.remote.size();
+        size += 8;
+        size += 1;
+        size += self.agent.len() as u32;
+        size += 5;
+        size
+    }
+
+    fn encode(&self) -> Buffer {
+        let mut buffer = Buffer::new();
+
         buffer.write_u32(self.version.as_u32()); //TODO protocol version should deref to a u32
-        buffer.write_u32(self.services.value()); //TODO services should return u32 not u64
+        buffer.write_u32(self.services.bits());
         buffer.write_u32(0);
         buffer.write_u64(self.time.timestamp() as u64);
         buffer.extend(self.remote.encode());
-        //Need to write the remote here. TODO probably impl an encode function for PeerAddr
+        buffer.extend(self.nonce.clone());
+        buffer.write_u8(self.agent.len() as u8);
+        buffer.write_str(&self.agent);
+        buffer.write_u32(self.height);
+        buffer.write_u8(self.no_relay as u8);
+
+        buffer
     }
-
-    // bw.writeU32(this.version);
-    // bw.writeU32(this.services);
-    // bw.writeU32(0);
-    // bw.writeU64(this.time);
-    // this.remote.write(bw);
-    // bw.writeBytes(this.nonce);
-    // bw.writeU8(this.agent.length);
-    // bw.writeString(this.agent, 'ascii');
-    // bw.writeU32(this.height);
-    // bw.writeU8(this.noRelay ? 1 : 0);
-
-    // assert((cmd & 0xff) === cmd);
-    // assert(Buffer.isBuffer(payload));
-    // assert(payload.length <= 0xffffffff);
-
-    // const msg = Buffer.allocUnsafe(9 + payload.length);
-
-    // // Magic value
-    // msg.writeUInt32LE(this.network.magic, 0, true);
-
-    // // Command
-    // msg[4] = cmd;
-
-    // // Payload length
-    // msg.writeUInt32LE(payload.length, 5, true);
-
-    // payload.copy(msg, 9);
-
-    // return msg;
-
-    pub fn frame() {}
 }
-
-// /**
-//  * Get serialization size.
-//  * @returns {Number}
-//  */
-// getSize() {
-//   let size = 0;
-//   size += 20;
-//   size += this.remote.getSize();
-//   size += 8;
-//   size += 1;
-//   size += this.agent.length;
-//   size += 5;
-//   return size;
-// }
-
-// /**
-//  * Write version packet to buffer writer.
-//  * @param {BufferWriter} bw
-//  */
-// write(bw) {
-//   bw.writeU32(this.version);
-//   bw.writeU32(this.services);
-//   bw.writeU32(0);
-//   bw.writeU64(this.time);
-//   this.remote.write(bw);
-//   bw.writeBytes(this.nonce);
-//   bw.writeU8(this.agent.length);
-//   bw.writeString(this.agent, 'ascii');
-//   bw.writeU32(this.height);
-//   bw.writeU8(this.noRelay ? 1 : 0);
-//   return this;
-// }
-
-// /**
-//  * Inject properties from buffer reader.
-//  * @private
-//  * @param {BufferReader} br
-//  */
-// read(br) {
-//   this.version = br.readU32();
-//   this.services = br.readU32();
-
-//   // Note: hi service bits
-//   // are currently unused.
-//   br.readU32();
-
-//   this.time = br.readU64();
-//   this.remote.read(br);
-//   this.nonce = br.readBytes(8);
-//   this.agent = br.readString(br.readU8(), 'ascii');
-//   this.height = br.readU32();
-//   this.noRelay = br.readU8() === 1;
-
-//   return this;
-// }
-// }
