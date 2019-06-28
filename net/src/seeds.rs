84607b1c6cbde5785dc8e20a93735064235d0537
kilpatty <seanpkilgarriff@gmail.com>
2019-06-27 20:36:23 -0500

    net-seeds: add port to seeds since netadress parse doesn't work

--- net/src/seeds.rs
+++ net/src/seeds.rs
@@ -1,13 +1,13 @@
 //TODO think about the best way to structure this.
-pub fn testnet_seed_nodes() -> Vec<&str> {
+pub fn testnet_seed_nodes() -> Vec<String> {
     vec![
-        "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189",
-        "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@173.255.209.126",
-        "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177",
-        "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168",
+        "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189:13038".to_owned(),
+        "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@173.255.209.126:13038".to_owned(),
+        "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177:13038".to_owned(),
+        "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168:13038".to_owned(),
     ]
 }
 
-pub fn mainnet_seed_nodes() -> Vec<&str> {
-    vec![""]
-}
+// pub fn mainnet_seed_nodes() -> Vec<&str> {
+//     vec![""]
+// }
