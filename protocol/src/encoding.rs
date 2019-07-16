5b97dc7ee5cf0e2e4875d1b427c3c517ff07a70a
kilpatty <seanpkilgarriff@gmail.com>
2019-07-16 17:14:04 -0500

    add custom errors, and make the error type only decoding

--- protocol/src/encoding.rs
+++ protocol/src/encoding.rs
@@ -1,7 +1,37 @@
-use handshake_types::Buffer;
+use extended_primitives::{Buffer, BufferError};
+use std::fmt;
+
+#[derive(Debug)]
+pub enum DecodingError {
+    Buffer(BufferError),
+    UnknownInvetory,
+}
+
+impl From<BufferError> for DecodingError {
+    fn from(e: BufferError) -> DecodingError {
+        DecodingError::Buffer(e)
+    }
+}
+
+impl fmt::Display for DecodingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            DecodingError::Buffer(ref e) => write!(f, "Buffer Error: {}", e),
+            DecodingError::UnknownInvetory => write!(f, "Unknown Inventory Type"),
+        }
+    }
+}
 
 pub trait Encodable {
     fn size(&self) -> u32;
 
     fn encode(&self) -> Buffer;
+}
+
+pub trait Decodable
+where
+    Self: Sized,
+{
+    type Error;
+    fn decode(buffer: &mut Buffer) -> Result<Self, Self::Error>;
 }
