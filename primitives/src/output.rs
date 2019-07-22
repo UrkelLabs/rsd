14a3c999cc5f64b9067880df99fd096cba68a7b1
kilpatty <seanpkilgarriff@gmail.com>
2019-07-22 10:59:29 -0500

    output: add encoding and decoding

--- primitives/src/output.rs
+++ primitives/src/output.rs
@@ -1,7 +1,9 @@
 use crate::{Address, Covenant};
+use extended_primitives::Buffer;
+use handshake_protocol::encoding::{Decodable, DecodingError, Encodable};
 use handshake_types::Amount;
 
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct Output {
     value: Amount,
     address: Address,
@@ -12,5 +14,38 @@ pub struct Output {
 impl Output {
     pub fn is_unspendable(&self) -> bool {
         self.address.is_unspendable() | self.covenant.is_unspendable()
+    }
+}
+
+impl Encodable for Output {
+    fn size(&self) -> u32 {
+        8 + self.address.size() + self.covenant.size()
+    }
+
+    fn encode(&self) -> Buffer {
+        let buffer = Buffer::new();
+
+        buffer.write_u64(self.value.as_doos());
+        buffer.extend(self.address.encode());
+        buffer.extend(self.covenant.encode());
+
+        buffer
+    }
+}
+
+impl Decodable for Output {
+    type Error = DecodingError;
+
+    fn decode(buffer: &mut Buffer) -> Result<Self, Self::Error> {
+        //TODO make from doos
+        let value = Amount::from_doo(buffer.read_u64()?);
+        let address = Address::decode(buffer)?;
+        let covenant = Covenant::decode(buffer)?;
+
+        Ok(Output {
+            value,
+            address,
+            covenant,
+        })
     }
 }
