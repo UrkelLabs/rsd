43e57750f1dd229a9d2e7d0e7971e45bb488d44b
kilpatty <seanpkilgarriff@gmail.com>
2019-07-23 11:52:52 -0500

    remove target....

--- script/target/rls/debug/deps/handshake_script-393d74a93880e30c.d
+++
@@ -1,5 +0,0 @@
-/Users/sean/Developer/HandshakeAlliance/rsd/script/target/rls/debug/deps/handshake_script-393d74a93880e30c.rmeta: script/src/lib.rs
-
-/Users/sean/Developer/HandshakeAlliance/rsd/script/target/rls/debug/deps/handshake_script-393d74a93880e30c.d: script/src/lib.rs
-
-script/src/lib.rs:
--- script/target/rls/debug/deps/handshake_script-527c99782e0390f8.d
+++
@@ -1,5 +0,0 @@
-/Users/sean/Developer/HandshakeAlliance/rsd/script/target/rls/debug/deps/handshake_script-527c99782e0390f8.rmeta: script/src/lib.rs
-
-/Users/sean/Developer/HandshakeAlliance/rsd/script/target/rls/debug/deps/handshake_script-527c99782e0390f8.d: script/src/lib.rs
-
-script/src/lib.rs:
