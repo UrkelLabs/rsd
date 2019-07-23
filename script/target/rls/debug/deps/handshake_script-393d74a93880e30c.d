e0496c9f52f87b2749c27625458d1fed66f4bd91
kilpatty <seanpkilgarriff@gmail.com>
2019-07-23 11:50:07 -0500

    script: initial

---
+++ script/target/rls/debug/deps/handshake_script-393d74a93880e30c.d
@@ -0,0 +1,5 @@
+/Users/sean/Developer/HandshakeAlliance/rsd/script/target/rls/debug/deps/handshake_script-393d74a93880e30c.rmeta: script/src/lib.rs
+
+/Users/sean/Developer/HandshakeAlliance/rsd/script/target/rls/debug/deps/handshake_script-393d74a93880e30c.d: script/src/lib.rs
+
+script/src/lib.rs:
