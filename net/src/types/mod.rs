2e4edc98877aa974da05b796490532ee722e71fb
kilpatty <seanpkilgarriff@gmail.com>
2019-07-24 16:03:19 -0500

    types: nonce

--- net/src/types/mod.rs
+++ net/src/types/mod.rs
@@ -2,8 +2,10 @@ pub mod identity_key;
 pub mod protocol_version;
 pub mod raw_ip;
 pub mod services;
+pub mod nonce;
 
 pub use identity_key::IdentityKey;
 pub use protocol_version::ProtocolVersion;
 pub use raw_ip::RawIP;
 pub use services::Services;
+pub use nonce::Nonce;
---
+++ net/src/types/nonce.rs
@@ -0,0 +1,2 @@
+//TODO maybe make this a [u8; 8], but u64 should be the exact same functionality.
+pub type Nonce = u64;
