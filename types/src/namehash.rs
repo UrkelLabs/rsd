9a4124350debf47ef70df9780d537f2ea186f604
kilpatty <seanpkilgarriff@gmail.com>
2019-08-27 12:21:01 -0500

    namehash: just wrap Hash inside of making a new struct

--- types/src/namehash.rs
+++ types/src/namehash.rs
@@ -1,6 +1,7 @@
 use extended_primitives::Hash;
 
 //Custom type specificly for Name Hashes.
-#[derive(PartialEq, Eq, Clone, Debug, Default)]
-pub struct NameHash(Hash);
+// #[derive(PartialEq, Eq, Clone, Debug, Default)]
+// pub struct NameHash(Hash);
 
+pub type NameHash = Hash;
