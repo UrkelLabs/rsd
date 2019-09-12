2d0edf6c93d40498f558dc4220cf942e94fd1938
kilpatty <seanpkilgarriff@gmail.com>
2019-09-12 13:48:33 -0500

    types: add encoding to time

--- types/src/time.rs
+++ types/src/time.rs
@@ -1,4 +1,6 @@
-use chrono::{DateTime, Utc};
+use chrono::Utc;
+use extended_primitives::Buffer;
+use handshake_protocol::encoding::{Decodable, DecodingError, Encodable};
 use std::cmp;
 use std::ops;
 
@@ -78,3 +80,27 @@ impl cmp::PartialOrd for Time {
 }
 
 //TODO impl From Datetime, SystemTime, Duration, and u64
+//
+impl Encodable for Time {
+    fn size(&self) -> u32 {
+        8
+    }
+
+    fn encode(&self) -> Buffer {
+        let mut buffer = Buffer::new();
+
+        buffer.write_u64(self.0);
+
+        buffer
+    }
+}
+
+impl Decodable for Time {
+    type Error = DecodingError;
+
+    fn decode(buffer: &mut Buffer) -> Result<Self, Self::Error> {
+        let inner = buffer.read_u64()?;
+
+        Ok(Self(inner))
+    }
+}
